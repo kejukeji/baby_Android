5dc3266c197fe901ab7de6d58a37d4e102b40859
ZhoujunAndroid <853173783@qq.com>
2013-10-29 10:54:54 +0800

    提交

--- baby/src/com/baby/service/PullService.java
+++ baby/src/com/baby/service/PullService.java
@@ -1,8 +1,6 @@
 package com.baby.service;
 
-import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
@@ -10,8 +8,6 @@ import android.os.IBinder;
 import android.os.Parcel;
 import android.os.RemoteException;
 
-import com.baby.R;
-import com.baby.activity.HomeActivity;
 import com.baby.helper.BusinessHelper;
 import com.baby.util.SharedPrefUtil;
 /**
@@ -93,28 +89,28 @@ public class PullService extends Service {
 
 		// The PendingIntent to launch our activity if the user selects this
 		// notification
-		Intent intent = new Intent(this, HomeActivity.class);
-		intent.putExtra("notifyId", notifyId);//消息id
-		intent.putExtra("title", title);//消息内容
+//		Intent intent = new Intent(this, HomeActivity.class);
+//		intent.putExtra("notifyId", notifyId);//消息id
+//		intent.putExtra("title", title);//消息内容
 		/**
 		 * requestCode 这个属性需要不一样，否则的话多个通知会指向相同的intent
 		 */
-		PendingIntent contentIntent = PendingIntent.getActivity(this, notifyId,intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//		PendingIntent contentIntent = PendingIntent.getActivity(this, notifyId,intent, PendingIntent.FLAG_UPDATE_CURRENT);
 		
 		// The ticker text, this uses a formatted string so our message could be
 		// localized
 
 		// construct the Notification object.
-		Notification notif = new Notification(R.drawable.ic_launcher, null,System.currentTimeMillis());
+//		Notification notif = new Notification(R.drawable.ic_launcher, null,System.currentTimeMillis());
 		//点击通知后自动从通知栏消失
-		notif.flags = Notification.FLAG_AUTO_CANCEL;
-		// Set the info for the views that show in the notification panel.
-		notif.setLatestEventInfo(this, title, content, contentIntent);
+//		notif.flags = Notification.FLAG_AUTO_CANCEL;
+//		// Set the info for the views that show in the notification panel.
+//		notif.setLatestEventInfo(this, title, content, contentIntent);
 
 		// after a 100ms delay, vibrate for 250ms, pause for 100 ms and
 		// then vibrate for 500ms.
 //		notif.vibrate = new long[] { 100, 250, 100, 500 };
-		notif.defaults = Notification.DEFAULT_SOUND;
-		mNM.notify(notifyId, notif);
+//		notif.efaults = Notification.DEFAULT_SOUND;
+//		mNM.notify(notifyId, notif);
 	}
 }
