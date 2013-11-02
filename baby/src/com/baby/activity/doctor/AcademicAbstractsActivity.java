fb21f63c6a3f73a55533774dfd9a3980d4c22e05
bwj <1154335879@qq.com>
2013-11-02 17:19:28 +0800

    bb

--- baby/src/com/baby/activity/doctor/AcademicAbstractsActivity.java
+++ baby/src/com/baby/activity/doctor/AcademicAbstractsActivity.java
@@ -42,6 +42,8 @@ public class AcademicAbstractsActivity extends BaseActivity {
 		list=setdata();
 		lvAcademic=(ListView)findViewById(R.id.lvAcademic);
 		academicAdapter=new AcademicAdapter(this,list);
+		lvAcademic.setAdapter(academicAdapter);
+		//lvAcademic.setVisibility(View.v)
 	}
 
 	private List<AcademicAbstractBean> setdata() {
@@ -69,19 +71,19 @@ public class AcademicAbstractsActivity extends BaseActivity {
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
-			return 0;
+			return list.size();
 		}
 
 		@Override
 		public Object getItem(int position) {
 			// TODO Auto-generated method stub
-			return null;
+			return position;
 		}
 
 		@Override
 		public long getItemId(int position) {
 			// TODO Auto-generated method stub
-			return 0;
+			return position;
 		}
 
 		@Override
@@ -100,6 +102,7 @@ public class AcademicAbstractsActivity extends BaseActivity {
 			viewHolder.content.setText(list.get(position).getContent());
 			return convertView;
 		}
+		
 	}
 	class ViewHolder{
 		public TextView title;
--- baby/src/com/baby/activity/doctor/DoctorMyActivity.java
+++ baby/src/com/baby/activity/doctor/DoctorMyActivity.java
@@ -1,40 +1,128 @@
 package com.baby.activity.doctor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 import com.baby.R;
 import com.baby.activity.base.BaseActivity;
+import com.baby.activity.doctor.AcademicAbstractsActivity.ViewHolder;
+import com.baby.bean.AcademicAbstractBean;
+import com.baby.bean.MyCollectBean;
 
 /**
  * 医生资料界面
  * @author Zhoujun
  * @version 创建时间：2013-10-25 下午2:53:44
  */
-public class DoctorMyActivity extends BaseActivity implements OnClickListener{
-	private Button btnChangeInform,btnChangePassword;
+public class DoctorMyActivity extends BaseActivity implements OnCheckedChangeListener{
+	private RadioGroup doctorMyRadioGroup;
+	private LinearLayout doctorMyLinearLayout;
+	private ListView doctorMyListView;
+	private MyCollectAdapter myCollectAdapter;
+	private List<MyCollectBean> list;
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.doctor_my_activity);
 		
-		btnChangeInform=(Button)findViewById(R.id.btnDoctor_my_change_inform);
-		btnChangePassword=(Button)findViewById(R.id.btnDoctor_my_change_pw);
-		btnChangeInform.setOnClickListener(this);
-		btnChangePassword.setOnClickListener(this);
+		list=setdata();
+		doctorMyRadioGroup=(RadioGroup)findViewById(R.id.doctor_my_radiogroup);
+		doctorMyLinearLayout=(LinearLayout)findViewById(R.id.doctor_my_linearlayout);
+		doctorMyListView=(ListView)findViewById(R.id.doctor_my_listview);
+        doctorMyRadioGroup.setOnCheckedChangeListener(this);
+        myCollectAdapter=new MyCollectAdapter(this, list);
+        doctorMyListView.setAdapter(myCollectAdapter);
+         
 	}
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.btnDoctor_my_change_inform:
-			openActivity(DoctorInfoEditActivity.class);
-			break;
 
+	private List<MyCollectBean> setdata() {
+		List<MyCollectBean> list=new ArrayList<MyCollectBean>();
+		MyCollectBean test=new MyCollectBean("新生儿的营养和需要量", " 新生儿出生后的2～4周内生长最快，按新生儿中等增长速度计算，每日增长体重在30克以上。新生儿补充营养的主要方式为：母乳喂养、混合喂养和人工喂养。新生儿期较其它各期相对营养素需要为高，为保证新生儿营养素的供给，减少或避免新生儿生理性体重减轻，应注意新生儿的营养供给量");
+		list.add(test);
+		
+		test=new MyCollectBean("新生儿的营养和需要量", " 新生儿出生后的2～4周内生长最快，按新生儿中等增长速度计算，每日增长体重在30克以上。新生儿补充营养的主要方式为：母乳喂养、混合喂养和人工喂养......");
+		list.add(test);
+		
+		return list;
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		switch (checkedId) {
+		case R.id.rbDoctor_my_collect:
+			doctorMyLinearLayout.setVisibility(View.GONE);
+		    doctorMyListView.setVisibility(View.VISIBLE);
+			break;
+		case R.id.rbDoctor_my_inform:
+			doctorMyLinearLayout.setVisibility(View.VISIBLE);
+			doctorMyListView.setVisibility(View.GONE);
+			break;
 		default:
 			break;
 		}
 		
+	}
+	class MyCollectAdapter extends BaseAdapter{
+		List<MyCollectBean> list=new ArrayList<MyCollectBean>();
+		private LayoutInflater mInflater;
+		public MyCollectAdapter(Context context,List<MyCollectBean> list) {
+			this.mInflater = LayoutInflater.from(context);
+			this.list=list;
+		}
+		
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return list.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder viewHolder=null;
+			if(convertView==null){
+				viewHolder=new ViewHolder();
+				convertView=mInflater.inflate(R.layout.doctor_my_collect_listitem, null);
+				viewHolder.title=(TextView)convertView.findViewById(R.id.my_collect_title);
+				viewHolder.content=(TextView)convertView.findViewById(R.id.my_collect_content);
+				convertView.setTag(viewHolder);
+			}else{
+				viewHolder=(ViewHolder)convertView.getTag();
+			}
+			viewHolder.title.setText(list.get(position).getTitle());
+			viewHolder.content.setText(list.get(position).getContent());
+			return convertView;
+		}
+		
+	}
+	class ViewHolder{
+		public TextView title;
+		public TextView content;
 	}
 }
